7eaab7386761d37ab13932d9b3480a8866dae78f
praneeth <praneethayla@gmail.com>
2023-08-24 19:54:24 +0530

    added to chromeExtension

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 let myLeads = [];
 let inputEl = document.querySelector("#input-el");
-
-
-
-
 let saveBtn = document.querySelector("#save-btn");
 
+let leadsFromStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromStorage)
+
 saveBtn.addEventListener("click", function() {
+
     if (inputEl.value.length >0) {
     let pushEL = inputEl.value
     myLeads.push(pushEL);
     inputEl.value = "";
+    localStorage.setItem("myLeads",JSON.stringify(myLeads));
+    // console.log(localStorage.getItem("myLeads"));
     renderLeads();
 }})
 
@@ -24,9 +26,7 @@ for (let i =0; i < myLeads.length; i++) {
             <a href = "${myLeads[i]}">${myLeads[i]}</a>
         </li>
         `
-    console.log(listItems)
 }
 ulEl.innerHTML = listItems; 
 }
-
 
