4a8efe90c528bfd9d82e3d027b61917aab48e2ec
praneeth <praneethayla@gmail.com>
2023-08-24 22:14:21 +0530

    saving tabs added

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const saveBtn = document.querySelector("#save-btn");
+const tabBtn = document.querySelector("#tab-btn");
 const leadsFromStorage = JSON.parse(localStorage.getItem("myLeads"));
 const deleteBtn = document.querySelector("#delete-btn");
 const ulEl = document.querySelector("#ul-el");
 
+
+tabBtn.addEventListener("click", function() {
+    // console.log(tabs[0].url)
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        renderLeads(myLeads)
+
+    })
+
+    
+})
+
+
 function renderLeads(leads) {
     let listItems = ""
-
     for (let i =0; i < leads.length; i++) {
         listItems +=
             `
@@ -33,13 +47,11 @@ if (leadsFromStorage) {
 }
 
 saveBtn.addEventListener("click", function() {
-
     if (inputEl.value.length >0) {
-    let pushEL = inputEl.value
-    myLeads.push(pushEL);
-    inputEl.value = "";
-    localStorage.setItem("myLeads",JSON.stringify(myLeads));
-    // console.log(localStorage.getItem("myLeads"));
-    renderLeads(myLeads);
+
+        myLeads.push(inputEl.value);
+        inputEl.value = "";
+        localStorage.setItem("myLeads",JSON.stringify(myLeads));
+        renderLeads(myLeads);
 }})
 
